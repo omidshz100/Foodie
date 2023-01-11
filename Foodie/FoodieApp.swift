1eb5d7befc914b8131cf4f59c57d2c359c28e8ce
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-01-11 11:54:52 +0100

    Initial Commit

---
+++ Foodie/FoodieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieApp.swift
+//  Foodie
+//
+//  Created by Omid Shojaeian Zanjani on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
