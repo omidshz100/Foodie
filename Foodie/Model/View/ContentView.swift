54fceee7d34316beb959212b1790a6f3b84ed087
Laura Bognanni <bognanni.laura@gmail.com>
2023-01-16 10:42:01 +0100

    test commit

--- Foodie/Model/View/ContentView.swift
+++ Foodie/Model/View/ContentView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView(){
-           // Ingredients()
-            Recipes()
+            Ingredients()
                 .tabItem{
                     Label("Pantry", systemImage: "cabinet")
                 }
--- Foodie/Model/View/Recipes.swift
+++ Foodie/Model/View/Recipes.swift
@@ -11,16 +11,19 @@ struct Recipes: View {
     var images = ["pic1", "pic2", "pic3", "pic4"]
     var recipes = ["Tortillas", "Churros", "Savory Pie Crust", "Vegan Pasta"]
     
+    //test commit
+    
     var body: some View {
-        ScrollView(){
-            ForEach(0..<images.count){image  in
-                RecipesRow(imageName: images[image], recipeName: recipes[image])
+        NavigationStack{
+            ScrollView(){
+                ForEach(0..<images.count){image  in
+                    RecipesRow(imageName: images[image], recipeName: recipes[image])
+                }
             }
+            .scrollIndicators(.hidden)
         }
-        .scrollIndicators(.hidden)
     }
 }
-
 struct Recipes_Previews: PreviewProvider {
     static var previews: some View {
         Recipes()
