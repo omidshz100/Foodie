df5b1321d775bac462caa89c280afe3b1be0d6b0
Laura Bognanni <bognanni.laura@gmail.com>
2023-01-24 12:39:51 +0100

    profile file updated

--- Foodie/Model/View/Profile.swift
+++ Foodie/Model/View/Profile.swift
@@ -9,7 +9,44 @@ import SwiftUI
 
 struct Profile: View {
     var body: some View {
-        Text("Grocery List")
+        VStack{
+            HStack(spacing: 30){
+                Image("NewPostProfile")
+                    .resizable()
+                    .frame(width: 100, height: 130)
+                VStack{
+                    Text("MrBroccoli")
+                    Text("broccoli23")
+                }
+                Spacer()
+            }
+            .padding(30)
+            
+            List {
+                VStack{
+                    Text("Notifications")
+                    //Spacer()
+                    Text("Notifications")
+                }
+                VStack{
+                    Text("Favourites")
+                    Text("Favourites")
+                }
+                VStack{
+                    Text("Settings")
+                    Text("Settings")
+                }
+                VStack{
+                    Text("Privacy")
+                    Text("Privacy")
+                }
+                VStack{
+                    Text("Screen Time")
+                    Text("Screen Time")
+                }
+            }
+            .listStyle(.plain)
+        }
     }
 }
 
