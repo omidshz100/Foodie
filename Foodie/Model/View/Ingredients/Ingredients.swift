90b4b61b258329fad2d6d8fed9cd8e6d7eb9cab2
giadapisani <giadapisani75@gmail.com>
2023-01-24 12:13:39 +0100

    ingredients view - buttons are cuter now

--- Foodie/Model/View/Ingredients/Ingredients.swift
+++ Foodie/Model/View/Ingredients/Ingredients.swift
@@ -41,7 +41,7 @@ struct Ingredients: View {
                 Spacer()
             }
             .padding()
-            TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: $text)
+            TextField("Search here", text: $text)
                 .padding(7)
                 .padding(.horizontal, 25)
                 .background(Color(.systemGray6))
@@ -60,6 +60,7 @@ struct Ingredients: View {
                         }, btnTitle: (self.apis.ingredients?.meals?[item].strIngredient ?? "NoName"))
                         .frame(width: 150)
                     }
+                    .padding(5)
                 }
             }
         }.onAppear(){
